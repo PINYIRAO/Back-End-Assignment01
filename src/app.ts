07204189aa6c9ec55ca002ae940a16330f895547
pinyirao <prao2@rrc.ca>
2025-01-12 05:30:21 -0600

    feat: finish Implementing Health Check Endpoints

--- src/app.ts
+++ src/app.ts
@@ -25,5 +25,25 @@ app.get("/", (req, res) => {
   res.send("Hello, welcome to Back-End world!");
 });
 
+// define a GET route for health check
+/**
+ * @openapi
+ * /api/v1/health:
+ *  get:
+ *   summary: Get health status of the application
+ *   tags: [Health]
+ *   responses:
+ *    200:
+ *     description: The application's status, uptime, the current timestamp, and version
+ */
+app.get("/api/v1/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  });
+});
+
 // export app and server for testing
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -9,3 +9,17 @@ describe("GET /", () => {
     expect(response.text).toMatch(/Hello.*world/i);
   });
 });
+
+describe("GET /api/v1/health", () => {
+  it("should return server health status", async () => {
+    // create GET request to health endpoint
+    const response: Response = await request(app).get("/api/v1/health");
+
+    // assert response status OK and health object to have specified properties
+    expect(response.status).toBe(200);
+    expect(response.body.status).toMatch(/ok/i);
+    expect(response.body.uptime).toBeGreaterThanOrEqual(0);
+    expect(response.body).toHaveProperty("timestamp");
+    expect(response.body).toHaveProperty("version");
+  });
+});
