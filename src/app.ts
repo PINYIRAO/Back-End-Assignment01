520609e0e35dbbbd78272770eae96b52799f9943
pinyirao <prao2@rrc.ca>
2025-01-12 05:02:23 -0600

    feat: finish API Documentation with Swagger implementation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 // import the express application and type definition
 import express, { Express } from "express";
 
+// import setupSwagger endpoint
+import setupSwagger from "../config/swagger";
+
 // initialize the express application
 const app: Express = express();
 
+// setup swagger for api documentation
+setupSwagger(app);
+
 // respond to GET request at endpoint "/" with message
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: get a greet message
+ *     tags: [Home]
+ *     responses:
+ *       200:
+ *         description: a greet message from application
+ */
 app.get("/", (req, res) => {
   res.send("Hello, welcome to Back-End world!");
 });
