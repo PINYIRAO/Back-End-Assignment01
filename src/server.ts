481457b1e3a4d0ea4e14039deebd54da0e3ec77c
pinyirao <prao2@rrc.ca>
2025-01-16 11:48:31 -0600

    fix: make a configuration for Eslint not to warn for the unused variable server

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { Server } from "http";
 const PORT: string | 3000 = process.env.PORT || 3000;
 
 // initialize server for the application to listen for requests on the specified port
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const server: Server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
